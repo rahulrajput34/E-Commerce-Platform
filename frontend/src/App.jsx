73113022d7a20862bff98c88662afc05b5e775f9
Rahul Rajput <rahul242rajput@gmail.com>
2024-10-02 14:13:28 -0400

    add Cart page and cart total component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,13 @@ import Login from './pages/Login'
 import Orders from './pages/Orders'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
       {/* This Navbar will be visible in all pages because we give above of the routes */}
       <Navbar/>
       <SearchBar/>
